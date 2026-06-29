34568daef6e90c936cbfba1c60ec21593370eee7
Dewan Shakil <mrdsa04@Dewans-MacBook-Pro.local>
2026-06-29 12:32:17 +0530

    Use native Metro resolution for benchmark app

--- examples/BareRNExample/metro.config.js
+++ examples/BareRNExample/metro.config.js
@@ -1,3 +1,7 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), {});
+module.exports = mergeConfig(getDefaultConfig(__dirname), {
+  resolver: {
+    resolverMainFields: ['react-native', 'main'],
+  },
+});
